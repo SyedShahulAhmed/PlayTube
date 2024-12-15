8090ddded44621dfb23f74ab58029e2595b216c3
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-12-15 09:55:57 +0530

    Added assests

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo from './logo.png'
+import menu from './menu.png'
+import addvideo from './addvideo.png'
+import box from './box.png'
+import bell from './bell.png'
+import profile from './profile.png'
+import search from './search.png'
+
+export const images = {
+    logo,menu,addvideo,bell,box,profile,search
+}

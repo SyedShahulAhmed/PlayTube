aa3b3ffcf4e39d7517ca15b8de7303511783fc1c
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-12-15 09:55:32 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl text-red-600'>Hello world</h1>
+      <NavBar/>
+      <Footer/>
     </div>
   )
 }

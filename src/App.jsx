ee40be5672042dfb33d11413eea81d040637f751
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-12-17 19:38:35 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Video from './pages/Video'
 const App = () => {
   return (
     <div>
       <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
       <Footer/>
     </div>
   )

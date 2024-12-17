810ba933c7c3ae308921f6177433402026af40b3
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-12-17 19:36:49 +0530

    Links and Channels Added

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,103 @@ import bell from './bell.png'
 import profile from './profile.png'
 import search from './search.png'
 
+//SideLinks
+
+import home from '../assets/SideLinks/home.png'
+import game from '../assets/SideLinks/game.png'
+import sports from '../assets/SideLinks/sports.png'
+import Blogs from '../assets/SideLinks/Blogs.png'
+import car from '../assets/SideLinks/car.png'
+import entertainment from '../assets/SideLinks/entertainment.png'
+import music from '../assets/SideLinks/music.png'
+import news from '../assets/SideLinks/news.png'
+import technology from '../assets/SideLinks/technology.png'
+
+//Channels
+
+import one from '../assets/Channels/one.png'
+import two from '../assets/Channels/two.png'
+import three from '../assets/Channels/three.png'
+import four from '../assets/Channels/four.png'
+import five from '../assets/Channels/five.png'
+
+
 export const images = {
     logo,menu,addvideo,bell,box,profile,search
 }
+
+export const SideLinks = [
+    {
+        id:1,
+        img:home,
+        name:"Home"
+    },
+    {
+        id:2,
+        img:game,
+        name:"Gaming"
+    },
+    {
+        id:3,
+        img:sports,
+        name:"Sports"
+    },
+    {
+        id:4,
+        img:entertainment,
+        name:"Entertainment"
+    },
+    {
+        id:5,
+        img:technology,
+        name:"Technology"
+    },
+    {
+        id:6,
+        img:car,
+        name:"Automoblies"
+    },
+    {
+        id:7,
+        img:music,
+        name:"Music"
+    },
+    {
+        id:8,
+        img:Blogs,
+        name:"Blogs"
+    },
+    {
+        id:9,
+        img:news,
+        name:"News"
+    },
+]
+
+export const Channels = [
+    {
+        id:1,
+        img:one,
+        channel:"MrBeast"
+    },
+    {
+        id:2,
+        img:two,
+        channel:"Chai aur Code"
+    },
+    {
+        id:3,
+        img:three,
+        channel:"Netflix"
+    },
+    {
+        id:4,
+        img:four,
+        channel:"PlayStation"
+    },
+    {
+        id:5,
+        img:five,
+        channel:"Shradha Khapra"
+    },
+]

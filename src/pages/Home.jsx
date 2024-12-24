18b00567e1a20972036a648d178bb7290337267e
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-12-24 21:15:43 +0530

    Modified Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import SideBar from '../components/SideBar'
+import React from "react";
+import SideBar from "../components/SideBar";
+import Feed from "../components/Feed";
+import Footer from "../components/Footer";
 
-const Home = () => {
+const Home = ({ Show }) => {
   return (
     <div>
-      <SideBar/>
+      <SideBar Show={Show} />
+      <div
+        className={`${
+          Show ? "pl-[16%]" : "pl-[8%]"
+        } bg-[#f9f9f9] pr-[2%] py-[20px]`}
+      >
+        <Feed />
+      </div>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

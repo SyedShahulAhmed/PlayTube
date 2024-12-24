47f7a09d21c3b2a5b91baa89f74d23f21a4f503e
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2024-12-24 21:14:19 +0530

    FeedPage Completed

---
+++ src/components/Feed.jsx
@@ -0,0 +1,127 @@
+import React from 'react'
+import sample from '../assets/default.png'
+import { Link } from 'react-router-dom'
+const Feed = () => {
+  return (
+    <div className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-x-[16px] gap-y-[30px] mt-[15px]'>
+<Link to={`video/20/4521`}>
+<div >
+       <img src={sample} alt="" className='w-full rounded-xl'/>
+        <h2 className='text-[16px] font-medium text-black my-1 mx-0'>Lorem ipsum dolor sit amet consectetur adipisicing </h2>
+        <h3 className='text-sm font-semibold text-[#555] my-[6px] mx-0'>Sample</h3>
+        <p className='text-sm'>20k Views &bull; 2 days ago</p>
+       </div>
+</Link>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+       <div>
+       <img src={sample} alt="" />
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, doloremque.</h2>
+        <h3>Sample</h3>
+        <p>20k Views &bull; 2 days ago</p>
+       </div>
+    </div>
+  )
+}
+
+export default Feed

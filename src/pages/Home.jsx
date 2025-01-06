dfa06f13efe77842d958a8eb71bb60a3f932fc6a
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-01-06 06:45:31 +0530

    Modified HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SideBar from "../components/SideBar";
 import Feed from "../components/Feed";
 import Footer from "../components/Footer";
 
 const Home = ({ Show }) => {
+  const [cat, setCat] = useState(0); // Rename to lowercase to avoid conflict with props
+
   return (
     <div>
-      <SideBar Show={Show} />
+      <SideBar Show={Show} cat={cat} setCat={setCat} />
       <div
         className={`${
           Show ? "pl-3 md:pl-[16%]" : "pl-[8%]"
         } bg-[#f9f9f9] pr-[2%] py-[20px]`}
       >
-        <Feed />
+        <Feed cat={cat} />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };

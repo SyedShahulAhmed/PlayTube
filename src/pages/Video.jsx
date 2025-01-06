625e6976689d46527c6275741450423c36814c90
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-01-06 06:46:23 +0530

    VideoPage Completed

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import VideoPlayer from '../components/VideoPlayer'
 import SideVideo from '../components/SideVideo'
+import { useParams } from 'react-router-dom'
 
 const Video = () => {
+
+  const {videoId,catId} = useParams()
+
   return (
-    <div className='bg-[#f9f9f9] pl-[2%] pr-[2%] pt-5 flex justify-between flex-wrap'>
-      <VideoPlayer/>
-      <SideVideo/>
+    <div className='bg-[#f9f9f9] pl-[2%] pr-[2%] pt-5 overflow-hidden flex justify-between flex-wrap'>
+      <VideoPlayer videoId={videoId} catId ={catId}/>
+      <SideVideo catId={catId}/>
     </div>
   )
 }
